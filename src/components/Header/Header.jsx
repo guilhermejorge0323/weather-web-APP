1f9a9a8f0f74691b1ea6613242eca6d3efc63044
Jorge <guilhermejorge272@gmail.com>
2026-03-11 02:40:17 -0300

    Parte do header pronto, faltando pequenos ajustes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
+import { FormHeader } from "./FormHeader"
 import { NavHeader } from "./NavHeader"
 
 export const Header = ({ isDark, setIsDark }) => {
     return (
         <header>
-            <NavHeader isDark={isDark} setIsDark={setIsDark}/>
+            <NavHeader isDark={isDark} setIsDark={setIsDark} />
+            <FormHeader />
         </header>
     )
 }
--- src/components/Header/NavHeader.jsx
+++ src/components/Header/NavHeader.jsx
@@ -2,7 +2,7 @@ import { Moon, Sun } from "lucide-react"
 
 export const NavHeader = ({ isDark, setIsDark }) => {
     return (
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center mb-6">
             <h1 className="text-3xl font-bold dark:text-white text-black md:text-4xl">Previsão do Tempo</h1>
             <button
                 className="dark:bg-yellow-400 dark:hover:bg-yellow-500 bg-slate-800 hover:bg-slate-900 rounded-full p-3 transition-all duration-300"
