77449b4bb1e5c9c7354dc479cc8e30495ba14727
Jorge <guilhermejorge272@gmail.com>
2026-03-20 04:18:30 -0300

    base dos cards de info das previsoe pronto

--- src/components/Main/Cards/Predictions/CardPerHours/CardInfoHours.jsx
+++ src/components/Main/Cards/Predictions/CardPerHours/CardInfoHours.jsx
@@ -1,1 +1,9 @@
-export const CardInfoHours = () =>  {}
+import { CardInfo } from "../CardInfo"
+
+export const CardInfoHours = () =>  {
+    return (
+        <CardInfo height={'140px'} width={'100px'}>
+            <h2>ola</h2>
+        </CardInfo>
+    )
+}
--- src/components/Main/Cards/Predictions/CardPerHours/CardPerHours.jsx
+++ src/components/Main/Cards/Predictions/CardPerHours/CardPerHours.jsx
@@ -1,12 +1,13 @@
 import { Card } from "../../Card"
 import { TitleCardPredictions } from "../TittleCardPredictions"
+import { CardInfoHours } from "./CardInfoHours"
 
 export const CardPerHours = () => {
     return (
         <Card>
             <TitleCardPredictions>Previsão por Hora</TitleCardPredictions>
             <div className="flex gap-4 overflow-x-auto pb-2">
-                
+                <CardInfoHours />
             </div>
         </Card>
     )
